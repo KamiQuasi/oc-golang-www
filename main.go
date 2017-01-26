17208e7fec6c44cb5a4a3d9c97028bb2b5de6024
Luke Dary <ldary@redhat.com>
2017-01-26 13:30:34 -0500

    Securing the service

--- main.go
+++ main.go
@@ -9,14 +9,21 @@ import (
 )
 
 func main() {
+	var err error
+	viper.BindEnv("cert", "SERVING_CERT")
+	viper.BindEnv("key", "SERVING_KEY")
 	viper.SetDefault("ip", "127.0.0.1")
-	viper.SetDefault("port", "8080")
+	viper.SetDefault("port", "4430")
 
 	http.HandleFunc("/", home)
 
 	bind := fmt.Sprintf("%s:%s", viper.Get("ip"), viper.Get("port"))
 	fmt.Printf("listening on %s...", bind)
-	http.ListenAndServe(bind, nil)
+	//http.ListenAndServe(bind, nil)
+	err = http.ListenAndServeTLS(bind, viper.GetString("cert"), viper.GetString("key"), nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
